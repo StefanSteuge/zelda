cd21a85c0bb80ec41345f7835e4cb50fc54420fc
Stepan Dulka <fotosteuge@gmail.com>
2023-10-17 20:20:53 +0300

    undo refactoring for ForrestScene

--- src/main/java/com/example/zelda/scene/ForrestScene.java
+++ src/main/java/com/example/zelda/scene/ForrestScene.java
@@ -7,7 +7,6 @@ import com.example.zelda.items.Rupee;
 import com.example.zelda.items.Warp;
 import com.example.zelda.karacter.Direction;
 
-import java.util.List;
 import java.awt.Polygon;
 import java.awt.Rectangle;
 
@@ -25,90 +24,105 @@ public class ForrestScene extends ZeldaScene {
     public ForrestScene(Game game, String entrance) {
         super(game, "/static/images/forrest-scene.png", "ForrestScene");
 
-        exits.addAll(List.of(exitRight1, exitRight2, exitUp, warpExit));
+        exits.add(exitRight1);
+        exits.add(exitRight2);
+        exits.add(exitUp);
+        exits.add(warpExit);
 
-        // Draw upperTreeline
-        var upperTreeline = new Polygon(new int[]{507, 485, 472, 451, 445, 426, 412, 388, 375, 354, 331, 317, 291, 271, 245, 219, 210, 197, 508, 508},
-                new int[]{182, 186, 192, 183, 167, 136, 89, 90, 135, 143, 103, 81, 80, 81, 81, 81, 81, 42, 44, 181},
-                20);
+        int[] axpos = {507, 485, 472, 451, 445, 426, 412, 388, 375, 354, 331, 317, 291, 271, 245, 219, 210, 197, 508, 508};
+        int[] aypos = {182, 186, 192, 183, 167, 136, 89, 90, 135, 143, 103, 81, 80, 81, 81, 81, 81, 42, 44, 181};
 
-        var upperTreeline2 = new Polygon(new int[]{203, 187, 180, 177, 328, 454, 506, 507, 361, 202},
-                new int[]{37, 16, 20, 4, 4, 4, 6, 39, 38, 37},
-                10);
+        var upperTreeline = new Polygon(axpos, aypos, aypos.length);
 
-        // Draw leftTreeline
-        var leftTreeline = new Polygon(new int[]{115, 110, 93, 88, 72, 96, 96, 98, 117, 119, 115, 110, 99, 88, 73, 74, 88, 96, 93, 92, 87, 77, 67, 5, 6, 115},
-                new int[]{4, 23, 52, 73, 93, 114, 127, 145, 162, 185, 199, 213, 219, 261, 271, 286, 297, 310, 332, 343, 360, 361, 396, 398, 5, 5},
-                26);
+        int[] cxpos = {203, 187, 180, 177, 328, 454, 506, 507, 361, 202};
+        int[] cypos = {37, 16, 20, 4, 4, 4, 6, 39, 38, 37};
 
-        var leftTreeline2 = new Polygon(new int[]{67, 60, 45, 71, 103, 120, 126, 142, 144, 5, 6, 66},
-                new int[]{398, 408, 429, 436, 435, 451, 482, 497, 509, 506, 398, 399},
-                12);
+        var upperTreeline2 = new Polygon(cxpos, cypos, cypos.length);
 
-        var middleTreeline = getPolygon();
+        int[] bxpos = {115, 110, 93, 88, 72, 96, 96, 98, 117, 119, 115, 110, 99, 88, 73, 74, 88, 96, 93, 92, 87, 77, 67, 5, 6, 115};
+        int[] bypos = {4, 23, 52, 73, 93, 114, 127, 145, 162, 185, 199, 213, 219, 261, 271, 286, 297, 310, 332, 343, 360, 361, 396, 398, 5, 5};
 
-        var entreeWall1 = new Polygon(new int[]{502, 452, 408, 393, 355, 354, 396, 413, 431, 436, 423, 400, 374, 401, 414, 463, 505, 504},
-                new int[]{236, 235, 280, 280, 317, 340, 383, 384, 362, 325, 340, 346, 320, 293, 293, 244, 246, 234},
-                18);
+        var leftTreeline = new Polygon(bxpos, bypos, bypos.length);
 
-        var entreeWall2 = new Polygon(new int[]{508, 483, 464, 462, 466, 477, 508, 508},
-                new int[]{345, 344, 329, 342, 363, 382, 385, 345},
-                8);
+        int[] dxpos = {67, 60, 45, 71, 103, 120, 126, 142, 144, 5, 6, 66};
+        int[] dypos = {398, 408, 429, 436, 435, 451, 482, 497, 509, 506, 398, 399};
 
-        var lowerWall = new Polygon(new int[]{508, 483, 464, 462, 466, 477, 508, 508, 509, 394, 365, 327, 295, 231, 197, 193, 508, 508},
-                new int[]{345, 344, 329, 342, 363, 382, 385, 345, 442, 443, 411, 408, 440, 443, 474, 510, 508, 441},
-                18);
+        var leftTreeline2 = new Polygon(dxpos, dypos, dypos.length);
 
-        var deadtree1 = new Polygon(new int[]{507, 477, 474, 505, 507},
-                new int[]{390, 389, 424, 425, 391},
-                5);
 
-        var deadtree2 = new Polygon(new int[]{272, 306, 306, 273, 272},
-                new int[]{322, 322, 352, 353, 322},
-                5);
+        int[] expos = {378, 346, 336, 321, 308, 311, 311, 292, 269, 245, 223, 211, 189, 163, 152, 157, 161, 175, 180, 163, 152, 157, 145, 130, 128, 133, 125, 117, 118, 136, 156, 180, 231, 255, 261, 263, 251, 236, 243, 276, 300, 307, 313, 322, 341, 347, 368, 390, 397, 400, 394, 379, 367};
+        int[] eypos = {179, 184, 183, 164, 161, 149, 132, 116, 115, 114, 114, 119, 114, 118, 138, 162, 183, 189, 207, 214, 230, 254, 260, 273, 293, 302, 308, 357, 372, 382, 376, 379, 380, 372, 357, 320, 308, 302, 279, 286, 279, 263, 238, 230, 232, 245, 254, 245, 231, 203, 188, 179, 179};
 
-        var deadtree3 = new Polygon(new int[]{153, 186, 186, 153, 153},
-                new int[]{478, 478, 508, 509, 481},
-                5);
+        var middleTreeline = new Polygon(expos, eypos, eypos.length);
 
-        solids.addAll(List.of(upperTreeline, leftTreeline, upperTreeline2, leftTreeline2, middleTreeline, entreeWall1, entreeWall2, lowerWall, deadtree1, deadtree2, deadtree3));
 
-        // add Link
+        int[] fxpos = {502, 452, 408, 393, 355, 354, 396, 413, 431, 436, 423, 400, 374, 401, 414, 463, 505, 504};
+        int[] fypos = {236, 235, 280, 280, 317, 340, 383, 384, 362, 325, 340, 346, 320, 293, 293, 244, 246, 234};
+
+        var entreeWall1 = new Polygon(fxpos, fypos, fypos.length);
+
+        int[] gxpos = {508, 483, 464, 462, 466, 477, 508, 508};
+        int[] gypos = {345, 344, 329, 342, 363, 382, 385, 345};
+
+        var entreeWall2 = new Polygon(gxpos, gypos, gypos.length);
+
+        int[] hxpos = {508, 483, 464, 462, 466, 477, 508, 508, 509, 394, 365, 327, 295, 231, 197, 193, 508, 508};
+        int[] hypos = {345, 344, 329, 342, 363, 382, 385, 345, 442, 443, 411, 408, 440, 443, 474, 510, 508, 441};
+
+        var lowerWall = new Polygon(hxpos, hypos, hypos.length);
+
+        int[] ixpos = {507, 477, 474, 505, 507};
+        int[] iypos = {390, 389, 424, 425, 391};
+
+        var deadTree1 = new Polygon(ixpos, iypos, iypos.length);
+
+        int[] jxpos = {272, 306, 306, 273, 272};
+        int[] jypos = {322, 322, 352, 353, 322};
+
+        var deadTree2 = new Polygon(jxpos, jypos, jypos.length);
+
+        int[] kxpos = {153, 186, 186, 153, 153};
+        int[] kypos = {478, 478, 508, 509, 481};
+
+        var deadTree3 = new Polygon(kxpos, kypos, kypos.length);
+
+        solids.add(upperTreeline);
+        solids.add(leftTreeline);
+        solids.add(upperTreeline2);
+        solids.add(leftTreeline2);
+        solids.add(middleTreeline);
+        solids.add(entreeWall1);
+        solids.add(entreeWall2);
+        solids.add(lowerWall);
+        solids.add(deadTree1);
+        solids.add(deadTree2);
+        solids.add(deadTree3);
+
         gameObjects.add(game.getLink());
 
-        gameObjects.addAll(List.of(
-                game.getLink(),
-                new BlueSoldier(game, 440, 375, Direction.UP, 20),
-                new BlueSoldier(game, 259, 403, Direction.RIGHT, 50),
-                new BlueSoldier(game, 137, 411, Direction.DOWN, 15),
-                new BlueSoldier(game, 128, 110, Direction.UP, 55),
-                new Rupee(game, 380, 110),
-                new Rupee(game, 390, 110),
-                new Rupee(game, 400, 110),
-                new Rupee(game, 410, 110),
-                new Rupee(game, 380, 128),
-                new Rupee(game, 390, 128),
-                new Rupee(game, 400, 128),
-                new Rupee(game, 410, 128),
-                new Warp(game, 393, 108)
-        ));
 
-        if (!game.getSong().equals("/static/sounds/overWorld.mp3")) {
+        gameObjects.add(game.getLink());
+        gameObjects.add(new BlueSoldier(game, 440, 375, Direction.UP, 20));
+        gameObjects.add(new BlueSoldier(game, 259, 403, Direction.RIGHT, 50));
+        gameObjects.add(new BlueSoldier(game, 137, 411, Direction.DOWN, 15));
+        gameObjects.add(new BlueSoldier(game, 128, 110, Direction.UP, 55));
+
+        gameObjects.add(new Rupee(game, 380, 110));
+        gameObjects.add(new Rupee(game, 390, 110));
+        gameObjects.add(new Rupee(game, 400, 110));
+        gameObjects.add(new Rupee(game, 410, 110));
+        gameObjects.add(new Rupee(game, 380, 128));
+        gameObjects.add(new Rupee(game, 390, 128));
+        gameObjects.add(new Rupee(game, 400, 128));
+        gameObjects.add(new Rupee(game, 410, 128));
+        gameObjects.add(new Warp(game, 393, 108));
+
+        if (!game.getSong().equals("/static/sounds/overworld.mp3")) {
             game.stopMusic();
-            game.playMusic("/static/sounds/overWorld.mp3", true);
+            game.playMusic("/static/sounds/overworld.mp3", true);
         }
 
         handleSwitchScene(entrance);
-    }
-
-    private static Polygon getPolygon() {
-        return new Polygon(new int[]{378, 346, 336, 321, 308, 311, 311, 292, 269, 245, 223, 211, 189, 163, 152, 157,
-                161, 175, 180, 163, 152, 157, 145, 130, 128, 133, 125, 117, 118, 136, 156, 180, 231, 255, 261, 263,
-                251, 236, 243, 276, 300, 307, 313, 322, 341, 347, 368, 390, 397, 400, 394, 379, 367},
-                new int[]{179, 184, 183, 164, 161, 149, 132, 116, 115, 114, 114, 119, 114, 118, 138, 162, 183, 189,
-                        207, 214, 230, 254, 260, 273, 293, 302, 308, 357, 372, 382, 376, 379, 380, 372, 357, 320,
-                        308, 302, 279, 286, 279, 263, 238, 230, 232, 245, 254, 245, 231, 203, 188, 179, 179},
-                57);
     }
 
     @Override
@@ -116,12 +130,15 @@ public class ForrestScene extends ZeldaScene {
         if (exit == exitRight1) {
             game.setScene(new HouseScene(game, "ForrestScene1"));
         }
+
         if (exit == exitRight2) {
             game.setScene(new HouseScene(game, "ForrestScene2"));
         }
+
         if (exit == exitUp) {
             game.setScene(new HyruleScene(game, "ForrestScene3"));
         }
+
         if (exit == warpExit) {
             game.setScene(new BattleScene(game, "warp"));
         }
@@ -129,22 +146,27 @@ public class ForrestScene extends ZeldaScene {
 
     @Override
     public void handleSwitchScene(String entrance) {
-        if (entrance.equals("HouseSceneLeft1")) {
+        if(entrance.equals("HouseSceneLeft1")) {
             moveScene(10, 1);
+
             game.getLink().setXHardCore(462);
             game.getLink().setYHardCore(195);
         }
-        if (entrance.equals("HouseSceneLeft2")) {
+
+        if(entrance.equals("HouseSceneLeft2")) {
             moveScene(10, 80);
+
             game.getLink().setXHardCore(459);
             game.getLink().setYHardCore(200);
         }
-        if (entrance.equals("HyruleScene")) {
+
+        if(entrance.equals("HyruleScene")) {
             moveScene(1, 1);
             game.getLink().setXHardCore(135);
             game.getLink().setYHardCore(31);
         }
-        if (entrance.equals("BattleScene")) {
+
+        if(entrance.equals("BattleScene")) {
             moveScene(10, 1);
             game.getLink().setXHardCore(382);
             game.getLink().setYHardCore(131);
